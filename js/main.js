e653b26310d60cb02fe8c056eea47b8f360f65ab
Nicolas Herrera <nicolas.herrera@autodesk.com>
2014-03-27 10:56:40 -0400

    Added main menu transitions

--- js/main.js
+++ js/main.js
@@ -1,58 +1,124 @@
 
 window.calamitoso = window.calamitoso || {};
 
-calamitoso.ui = function(){
+calamitoso.ui = function($){
 
     'use strict';
 
-    //private vars
+    //private members
     var ns      = calamitoso.ui || {},
         cache   = {
-            'mainNav'       : $('.main-nav'),
-            'mainNavItems'  : $('.main-nav-item')
-        };
+            'mainNav'           : $('.main-nav'),
+            'mainNavItems'      : $('.main-nav-item')
+        },
+        touchEnabled = ( 'ontouchstart' in window || 'onmsgesturechange' in window );
 
-    //public vars
-    ns.mainNav = {};
-
-    //define functions
-    ns.init = function(){
-
-        calamitoso.ui.mainNav.isHome = true;
-
-        cache.mainNavItems.on('click', function(e){
-            calamitoso.ui.mainNav.toggle();
-            e.preventDefault();
-        });
+    //public members
+    ns.getTouchEnabled = function(){
+        return touchEnabled;
     };
 
-    ns.mainNav.toggle = function(){
-        console.log('mainNav toggle');
+    //main navigation module
+    (function(){
 
-        console.log(this);
+        //private members
+        var _isHome = false,
+            toggle = function(){
+                _isHome = !_isHome;
 
-        this.isHome = !this.isHome;
+                if(_isHome){
+                    //send to center
+                    cache.mainNav.removeClass('main-nav-is-parked');
+                }else{
+                    //park on the side
+                    cache.mainNav.addClass('main-nav-is-parked');
+                }
+            };
 
-        if(this.isHome){
-            //send to center
-            cache.mainNav.removeClass('parked');
-        }else{
-            //park on the side
-            cache.mainNav.addClass('parked');
+        //public members
+        ns.mainNav = {};
+
+        // ns.mainNav.setIsHome = function(value){
+        //     _isHome = Boolean(value);
+        //     return _isHome;
+        // };
+
+        // var onItemClick = function(e){
+        //     calamitoso.ui.mainNav.toggle();
+        //     e.preventDefault();
+        // };
+
+        //expose nav init
+        ns.mainNav.init = function(){
+
+            //set initial main nav state
+            _isHome = true;
+
+            //attach event listeners to buttons
+            cache.mainNavItems.on('click', function(e){
+
+                if( calamitoso.ui.getTouchEnabled() ){
+                    //hijack hover actions
+                   //$(this).addClass('main-nav-item-no-hover');
+                }else{
+                    toggle();
+                }
+
+                e.preventDefault();
+            });
+
+
         }
 
-    };
+        // ns.mainNav.toggle = function(){
+
+        //     _isHome = !_isHome;
+
+        //     if(_isHome){
+        //         //send to center
+        //         cache.mainNav.removeClass('main-nav-is-parked');
+        //     }else{
+        //         //park on the side
+        //         cache.mainNav.addClass('main-nav-is-parked');
+        //     }
+        // };
+
+    }());
+
+    //app initialization module
+    (function(){
+
+        //private memebers
+        var _init = function(){
+            //append capabilities classes
+            var className = calamitoso.ui.getTouchEnabled() ? 'touch' : 'no-touch';
+            $('html').addClass(className);
+            //initialize main nav module
+            calamitoso.ui.mainNav.init();
+            //unregister init function
+            delete calamitoso.ui.init;
+        }
+
+        //public members
+        //expose init method
+        ns.init = function(){
+            _init();
+        };
+
+    }());
 
     return ns;
 
-}();
+}(jQuery);
 
 
 $(function() {
-    // $('.main-nav-item').on('click', function(e){
-    //     console.log($(this));
-    //     e.preventDefault();
-    // });
+
+    //testing overwrite
+    // calamitoso.ui.getTouchEnabled = function(){
+    //     return true;
+    // };
+
     calamitoso.ui.init();
     console.log(calamitoso);
 });
